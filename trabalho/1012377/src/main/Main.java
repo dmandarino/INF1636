1f758bd6f3a9b4883bdb4ef3916fd02e9ea0a908
npaiva <npaiva@outlook.com>
2014-10-20 15:57:46 -0200

    teste do projeto

---
+++ trabalho/1012377/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import modelos.Peca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Peca p = new Peca();
+		
+		p.print();
+	}
+
+}
---
+++ trabalho/1012377/src/modelos/Peca.java
@@ -0,0 +1,7 @@
+package modelos;
+
+public class Peca {
+	public void print(){
+		System.out.println("hello wolrd!");
+	}
+}
