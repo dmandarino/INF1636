9879d43de6f5274387e4a3413ded2fa9dbaf4011
npaiva <npaiva@outlook.com>
2014-10-20 16:23:46 -0200

    mudanca

--- trabalho/1012377/src/modelos/Peca.java
+++ trabalho/1012377/src/modelos/Peca.java
@@ -1,6 +1,17 @@
 package modelos;
 
 public class Peca {
+	
+	private String figura;
+	
+	public String getFigura() {
+		return figura;
+	}
+
+	public void setFigura(String figura) {
+		this.figura = figura;
+	}
+
 	public void print(){
 		System.out.println("hello wolrd!");
 	}
