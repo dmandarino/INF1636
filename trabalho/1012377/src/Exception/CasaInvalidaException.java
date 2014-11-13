d3678cd8179b0fc6aee0d0f95db3a5df3cc86f4f
dmandarino <mandarino.douglas@me.com>
2014-11-13 19:29:14 -0200

    Renomeando

--- trabalho/1012377/src/Exception/CasaInvalidaException.java
+++
@@ -1,8 +0,0 @@
-package Exception;
-
-public class CasaInvalidaException extends Exception {
-	public CasaInvalidaException() {
-		// TODO Auto-generated constructor stub
-		System.out.println("Casa Invalida");
-	}
-}
