c57a6ed7c5df7f31067200c73d9f0db2a8407529
dmandarino <mandarino.douglas@me.com>
2014-11-15 03:41:52 -0200

    adicionando Rei e Rainha (movimentos)

--- trabalho/1012377/src/service/MovimentoRainha.java
+++ trabalho/1012377/src/service/MovimentoRainha.java
@@ -4,25 +4,88 @@ import java.util.HashMap;
 
 import modelos.Casa;
 import modelos.Rainha;
+import Exception.CasaOcupadaException;
+import Exception.MoimentoInvalidoException;
 
 public class MovimentoRainha implements Movimento<Rainha>{
 
+	private static final int CIMA = -7;
+	private static final int DIR = 1;
+	private static final int ESQ = -1;
+	private static final int BAIXO = 7;
+	private static final int BAIXO_DIR = 8;
+	private static final int BAIXO_ESQ = 6;
+	private static final int CIMA_DIR = -6;
+	private static final int CIMA_ESQ = -8;
+	
+	
 	@Override
-	public void andar(Rainha e, Casa c, HashMap<Integer, Casa> casas) {
-		// TODO Auto-generated method stub
-		
+	public void andar(Rainha rainha, Casa casaDestino, HashMap<Integer, Casa> casas) {
+		try {
+			if ( isCasaOcupada(casaDestino))
+				throw new CasaOcupadaException();
+			
+			Integer direcao = getDirecao(rainha, casaDestino);
+			
+			while(pecaNaoEstaNaCasa(rainha, casaDestino)){
+				if ( isCasaOcupada(casas.get(rainha.getCasa().getNumCasa() + direcao)))
+					throw new CasaOcupadaException();
+				if(movimentoValido(rainha, casas.get(rainha.getCasa().getNumCasa() + direcao))){
+					casas.get(rainha.getCasa().getNumCasa()+1).setPeca(null);
+					rainha.setCasa(casas.get(rainha.getCasa().getNumCasa() + direcao));
+					casas.get(rainha.getCasa().getNumCasa()).setPeca(rainha);
+				}
+				else throw new MoimentoInvalidoException();
+			}
+		} catch (CasaOcupadaException e) {
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private boolean isMovDiagonal(Casa casaDestino, Rainha rainha) {
+		return !casaDestino.getX().equals(rainha.getCasa().getX()) && !casaDestino.getY().equals(rainha.getCasa().getY()); 
+	}
+
+	private Integer getDirecao(Rainha rainha, Casa casaDestino) {
+		if (isMovDiagonal(casaDestino, rainha)){
+			if(casaDestino.getX() > rainha.getCasa().getX()){
+				if(casaDestino.getY() > rainha.getCasa().getY() ){
+					return CIMA_DIR;
+				} else {
+					return BAIXO_DIR;
+				}
+			} else if (casaDestino.getY() > rainha.getCasa().getY()){
+				return CIMA_ESQ;
+			}
+			return BAIXO_ESQ;
+		} 
+		if(casaDestino.getX().equals(rainha.getCasa().getX())){
+			if(casaDestino.getY() > rainha.getCasa().getY() ){
+				return BAIXO;
+			} else {
+				return CIMA;
+			}
+		} else if (casaDestino.getX() > rainha.getCasa().getX()){
+			return DIR;
+		}
+		return ESQ;
+	}
+	
+	private boolean pecaNaoEstaNaCasa(Rainha rainha, Casa casaDestino) {
+		return !rainha.getCasa().getNumCasa().equals(casaDestino.getNumCasa());
 	}
 
 	@Override
-	public boolean movimentoValido(Rainha e, Casa c) {
-		// TODO Auto-generated method stub
-		return false; 
+	public boolean movimentoValido(Rainha rainha, Casa c) {
+		return rainha.getCasa().getX().equals(c.getX()) || rainha.getCasa().getY().equals(c.getY()) ||
+				(c.getX().equals(rainha.getCasa().getX()) && c.getY().equals(rainha.getCasa().getY()));
 	}
 
 	@Override
-	public boolean isCasaOcupada(Casa c) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isCasaOcupada(Casa casa) {
+		return casa.getPeca() != null;
 	}
+
 
 }
--- trabalho/1012377/src/service/MovimentoRei.java
+++ trabalho/1012377/src/service/MovimentoRei.java
@@ -4,25 +4,81 @@ import java.util.HashMap;
 
 import modelos.Casa;
 import modelos.Rei;
+import Exception.CasaOcupadaException;
+import Exception.MoimentoInvalidoException;
 
 public class MovimentoRei implements Movimento<Rei>{
-
+	
+	private static final int CIMA = -7;
+	private static final int DIR = 1;
+	private static final int ESQ = -1;
+	private static final int BAIXO = 7;
+	private static final int BAIXO_DIR = 8;
+	private static final int BAIXO_ESQ = 6;
+	private static final int CIMA_DIR = -6;
+	private static final int CIMA_ESQ = -8;
+	
+	
 	@Override
-	public void andar(Rei e, Casa c, HashMap<Integer, Casa> casas) {
-		// TODO Auto-generated method stub
-		
+	public void andar(Rei rei, Casa casaDestino, HashMap<Integer, Casa> casas) {
+		try {
+			if ( isCasaOcupada(casaDestino))
+				throw new CasaOcupadaException();
+			
+			Integer direcao = getDirecao(rei, casaDestino);
+			
+			if ( isCasaOcupada(casas.get(rei.getCasa().getNumCasa() + direcao)))
+				throw new CasaOcupadaException();
+			if(movimentoValido(rei, casas.get(rei.getCasa().getNumCasa() + direcao))){
+				casas.get(rei.getCasa().getNumCasa()+1).setPeca(null);
+				rei.setCasa(casas.get(rei.getCasa().getNumCasa() + direcao));
+				casas.get(rei.getCasa().getNumCasa()).setPeca(rei);
+			}
+			else throw new MoimentoInvalidoException();
+		} catch (CasaOcupadaException e) {
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private boolean isMovDiagonal(Casa casaDestino, Rei rei) {
+		return !casaDestino.getX().equals(rei.getCasa().getX()) && !casaDestino.getY().equals(rei.getCasa().getY()); 
+	}
+
+	private Integer getDirecao(Rei rei, Casa casaDestino) {
+		if (isMovDiagonal(casaDestino, rei)){
+			if(casaDestino.getX() > rei.getCasa().getX()){
+				if(casaDestino.getY() > rei.getCasa().getY() ){
+					return CIMA_DIR;
+				} else {
+					return BAIXO_DIR;
+				}
+			} else if (casaDestino.getY() > rei.getCasa().getY()){
+				return CIMA_ESQ;
+			}
+			return BAIXO_ESQ;
+		} 
+		if(casaDestino.getX().equals(rei.getCasa().getX())){
+			if(casaDestino.getY() > rei.getCasa().getY() ){
+				return BAIXO;
+			} else {
+				return CIMA;
+			}
+		} else if (casaDestino.getX() > rei.getCasa().getX()){
+			return DIR;
+		}
+		return ESQ;
 	}
 
 	@Override
-	public boolean movimentoValido(Rei e, Casa c) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean movimentoValido(Rei rei, Casa c) {
+		return rei.getCasa().getX().equals(c.getX()) || rei.getCasa().getY().equals(c.getY()) ||
+				(c.getX().equals(rei.getCasa().getX()) && c.getY().equals(rei.getCasa().getY()));
 	}
 
 	@Override
-	public boolean isCasaOcupada(Casa c) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isCasaOcupada(Casa casa) {
+		return casa.getPeca() != null;
 	}
 
 }
